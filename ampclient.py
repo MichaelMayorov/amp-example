b71e03f91bb3c930c8fd4437fe0496691d8aa00d
Michael Mayorov <marchael@kb.csu.ru>
2013-06-20 01:49:37 +0600

    Trying to replace 2 connections to one, for now not working

--- ampclient.py
+++ ampclient.py
@@ -9,26 +9,28 @@ from protocols import GetInfo, SetBuilderList
 
 def doConnection():
     creator = ClientCreator(reactor, amp.AMP)
-    getInfo = creator.connectTCP('127.0.0.1', 1234)
-    def connected(ampProto):
+    conn = creator.connectTCP('127.0.0.1', 1234)
+    def getInfoConnected(ampProto):
         return ampProto.callRemote(GetInfo)
-    getInfo.addCallback(connected)
+    conn.addCallback(getInfoConnected)
 
-    setBuilderList = creator.connectTCP('127.0.0.1', 1234)
-    def connected(ampProto):
+    def setBuilderListConnected(ampProto):
         builders = [
             {'name': 'python2.7', 'dir': '/build/py2.7'},
             {'name': 'python3', 'dir': '/build/py3'}
             ]
-
         return ampProto.callRemote(SetBuilderList, builders=builders)
-    setBuilderList.addCallback(connected)
+    conn.addCallback(setBuilderListConnected)
 
 
-    def done(result):
+    def getInfoDone(result):
         log.msg('Slave info: %s' % pprint.pformat(result))
-        reactor.stop()
-    getInfo.addCallback(done)
+    conn.addCallback(getInfoDone)
+    def setBuilderListDone(result):
+        log.msg('Slave setBuilderList result: %d' % result)
+    conn.addCallback(setBuilderListDone)
+    reactor.stop() # kills connection even if one of this function uses it
+
 
 def main():
     doConnection()
