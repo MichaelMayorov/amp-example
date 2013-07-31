edbaf5b11017619f7de8f8adc1a071caec31aaa8
Michael Mayorov <marchael@kb.csu.ru>
2013-08-01 00:40:42 +0600

    Upload file from slave, removed old startCommand code

--- fake_master.py
+++ fake_master.py
@@ -10,7 +10,7 @@ from protocols import DebugAMP
 from twisted.internet.endpoints import TCP4ClientEndpoint
 from protocols import GetInfo, SetBuilderList, RemotePrint, RemoteStartCommand, RemoteAcceptLog,\
 RemoteAuth, RemoteInterrupt, RemoteSlaveShutdown, ShellBbCommand, RemoteUpdateSendRC,\
-MkdirBbCommand
+MkdirBbCommand, Chunk, EndData, UploadFileBbCommand
 
 
 @defer.inlineCallbacks
@@ -24,24 +24,21 @@ def setBuilders(ampProto):
         {'name': 'python2.7', 'dir': 'py2.7'},
         {'name': 'python3', 'dir': 'py3'}
         ]
-    builderListResult = yield ampProto.callRemote(SetBuilderList, builders=builders) 
+    builderListResult = yield ampProto.callRemote(SetBuilderList, builders=builders)
+    ampProto.builders = {
+        'python2.7': {
+            'dir': 'py2.7',
+        },
+        'python3': {
+            'dir': 'py3',
+        },
+    }
     defer.returnValue(builderListResult)
 
 @defer.inlineCallbacks
 def remotePrint(ampProto):
     message = "Just test message from master"
     res = yield ampProto.callRemote(RemotePrint, message=message)
-    defer.returnValue(res)
-
-@defer.inlineCallbacks
-def remoteStartCommand(ampProto):
-    environ = [{'key': 'foo', 'value': 'bar'}, {'key': 'baz', 'value': 'quux'}]
-    cmd = '/bin/ls'
-    args = ['-la', '/']
-    builder = 'py2.7'
-    res = yield ampProto.callRemote(RemoteStartCommand, environ=environ, command=cmd,
-        args=args, builder=builder
-    )
     defer.returnValue(res)
 
 @defer.inlineCallbacks
@@ -64,24 +61,42 @@ def remoteRunMkdirCommand(ampProto):
     defer.returnValue(error)
 
 @defer.inlineCallbacks
+def uploadFile(ampProto):
+    builder = 'python2.7'
+    slavesrc = 'file'
+    builder_info = ampProto.builders.get(builder)
+    if builder_info.get('file') is not None:
+        defer.returnValue({'error': 'Uploading in progress, wait when it\'s done'})
+    error = yield ampProto.callRemote(UploadFileBbCommand,
+        builder=builder, workdir='test-upload', slavesrc=slavesrc,
+        maxsize=0, keepstamp=False
+    )
+    if error['error'] != '': # upload failed
+        builder_info['file'] = None
+    else:
+        builder_info['file'] = slavesrc
+    defer.returnValue(error)
+
+@defer.inlineCallbacks
 def Hello(ampProto):
     info, builderListResult = yield defer.gatherResults([
             getInfo(ampProto),
             setBuilders(ampProto)
     ])
     remPrintRes = yield remotePrint(ampProto)
-    # remStartCmd = yield remoteStartCommand(ampProto) # not function
     shellCmdErr = yield remoteRunShellCommand(ampProto)
     mkdirErr = yield remoteRunMkdirCommand(ampProto)
+    uploadErr = yield uploadFile(ampProto)
 
     log.msg('Slave info: %s' % pprint.pformat(info))
     log.msg('Slave setBuilderList result: %s' % builderListResult)
     log.msg('Remote print result: %s' % remPrintRes)
-    # log.msg('Remote execution\'s result: %s' % pprint.pformat(remStartCmd))
     if shellCmdErr['error'] != '':
         log.msg('Error while trying to execute shell command on slave: "%s"' % shellCmdErr)
     if mkdirErr['error'] != '':
         log.msg('Error while trying to execute mkdir command on slave: "%s"' % mkdirErr)
+    if uploadErr['error'] != '':
+        log.msg('Error while uploading file to master: "%s"' % uploadErr)
 
     yield ampProto.callRemote(RemoteInterrupt, command='ls')
     yield ampProto.callRemote(RemoteSlaveShutdown)
@@ -114,6 +129,30 @@ class Master(DebugAMP):
     @RemoteUpdateSendRC.responder
     def remoteUpdateRC(self, builder, rc):
         log.msg('Slave builder: "%s" done command with rc: %d' % (builder, rc))
+        return {}
+
+    @Chunk.responder
+    def receiveChunk(self, builder, chunk):
+        builder_info = self.builders.get(builder)
+        if builder_info is None:
+            return {}
+        writer = builder_info.get('writer')
+        if writer is None:
+            # file not exists
+            writer = open(builder_info['file'], 'wb')
+            builder_info['writer'] = writer
+        writer.write(chunk)
+        return {}
+
+    @EndData.responder
+    def closeWriter(self, builder):
+        builder_info = self.builders.get(builder)
+        if builder_info is None:
+            return {}
+        if builder_info.get('writer') is not None:
+            builder_info['writer'].close()
+            builder_info['writer'] = None
+        builder_info['file'] = None
         return {}
 
 def main():
